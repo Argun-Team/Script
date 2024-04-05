c659cdf8d0e025dd5d42e3be01fc870e210f945b
niuniujun <niuniujun@argun.cc>
2024-04-05 12:20:58 +0800

    更新Chipper Surge

--- ChipperCash/Surge/js/getData.js
+++ ChipperCash/Surge/js/getData.js
@@ -1,21 +1,17 @@
 let req = $request;
 if (req && req.headers) {
-    // 提取请求头中的 authorization，并只获取 Bearer 之后的内容
-    let authorizationFull = req.headers['Authorization'] || req.headers['authorization'];
-    let authorization = authorizationFull ? authorizationFull.split(" ")[1] : ""; // 假设总是存在 Bearer
-    let deviceId = req.headers['chipper-device-id'] || req.headers['Chipper-Device-Id'];
+    // 提取请求头中的 authorization 和 chipper-device-id
+    let authorization = req.headers['authorization'];
+    let deviceId = req.headers['chipper-device-id'];
 
     // 构造邮件正文内容
-    let mailBody = `Authorization (Bearer): ${authorization}\nChipper-Device-Id: ${deviceId}`;
+    let mailBody = `Authorization: ${authorization}\nChipper-Device-Id: ${deviceId}`;
 
-    // 对邮件正文进行编码
-    let encodedMailBody = encodeURIComponent(mailBody);
-
-    // 构造 mailto: 链接，确保邮件主体被正确编码
-    let mailtoUrl = `mailto:your-email@example.com?subject=ChipperCash Authorization&body=${encodedMailBody}`;
+    // 构造 mailto: 链接
+    let mailtoUrl = `mailto:your-email@example.com?subject=ChipperCash Authorization&body=${encodeURIComponent(mailBody)}`;
 
     // 发送通知
-    $notification.post("ChipperCash Authorization", "点击查看详情", "📧 发送到邮箱", { "open-url": mailtoUrl });
+    $notification.post("ChipperCash Authorization", "点击查看详情", "📧 发送到邮箱", { "url": mailtoUrl });
 
     $done({});
 } else {
