30b25af0a45bb8219483933b924e8a57130416aa
niuniujun <niuniujun@argun.cc>
2024-05-29 23:33:38 +0800

    update

--- ChipperCash/Surge/js/skipFrozen.js
+++ ChipperCash/Surge/js/skipFrozen.js
@@ -1,6 +1,9 @@
-// 假设 isFrozen为false跳过人脸认证
-$done({
-    body: JSON.stringify({
-        "isFrozen": false
-    })
-});
+var body = $response.body; // 获取响应体
+var obj = JSON.parse(body); // 将响应体解析为JSON对象
+
+
+obj.identityVerification.verificationSteps.liveness.status = "NOT_STARTED";
+
+body = JSON.stringify(obj); // 将修改后的JSON对象转换回字符串
+
+$done({body}); // 返回修改后的响应体
